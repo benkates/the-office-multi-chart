a94885266aaf2a1db8c834b740309c6e7efe2419
Ben Kates <benekates@gmail.com>
2022-12-26 14:46:25 -0500

    change click function

--- src/utils/clickFun.js
+++ src/utils/clickFun.js
@@ -1,25 +1,12 @@
-import { fullText } from "./data";
+import catData from "../data/nodes.json";
 export default function clickFun(d, i, marker) {
   //if it's a keypress and it's not the enter key, return
   if (d.type === "keydown" && d.key !== "Enter") {
     return;
   }
 
-  //split words into an array
-  let words = fullText.split(" ");
+  let keyword = catData[i].name;
 
-  //get a random index of the array
-  let indexNum = Math.floor(Math.random() * words.length);
-
-  //get the actual keyword of that
-  let keyword = words[indexNum];
-
-  //randomly decide to add another word
-  Math.round(Math.random()) === 1
-    ? (keyword = `${keyword} ${words[indexNum + 1]}`)
-    : null;
-
-  //add mark
   marker.mark(keyword, {
     className: `group-${i}`,
     accuracy: "exactly",
